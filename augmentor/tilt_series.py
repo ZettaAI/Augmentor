0b4e038befae2c924e9c80446b376e02e0a92bc7
Kisuk Lee <kisuk@princeton.edu>
2022-05-17 20:15:36 +0900

    TiltSeries initial commit

--- augmentor/tilt_series.py
+++ augmentor/tilt_series.py
@@ -50,42 +50,98 @@ class TiltedView(object):
     @staticmethod
     def project(img, n, dir=1, axis_t=-1, axis_p=-3):
         assert img.shape[axis_p] % n == 0
+        assert (dir == 1) or (dir == -1)
         prj = np.copy(img)
-        for i in range(1, n):
+        pivot = n//2
+        for i in range(0, n):
+            if i == pivot:
+                continue
             idx = [slice(None)] * img.ndim
             idx[axis_p] = slice(i, img.shape[axis_p], n)
             idx = tuple(idx)
-            prj[idx] = np.roll(img[idx], dir*i, axis=axis_t)
-        return NormalView.project(prj, n, axis=axis_p)
+            prj[idx] = np.roll(img[idx], dir*(i - pivot), axis=axis_t)
+        prj = NormalView.project(prj, n, axis=axis_p)
+        
+        # Invalidate the first half
+        idx_l = [slice(None)] * img.ndim
+        idx_l[axis_t] = slice(0, pivot)
+        prj[idx_l] = 0
+        
+        # Invalidate the second half
+        idx_r = [slice(None)] * img.ndim
+        idx_r[axis_t] = slice(-pivot, None)        
+        prj[idx_r] = 0
+
+        return prj
 
 
+def simple_projections(num_sections): 
+    prjs = list()
+    prjs.append(NormalView(num_sections))
+    prjs.append(TiltedView(num_sections,  1, -1))  # y + 45
+    prjs.append(TiltedView(num_sections, -1, -1))  # y - 45
+    prjs.append(TiltedView(num_sections,  1, -2))  # x + 45
+    prjs.append(TiltedView(num_sections, -1, -2))  # x - 45
+    return prjs
+
+
+class SimpleTiltSeries(object):
+    """
+    Tilt-series projections.
+    """
+    def __init__(self, num_sections):
+        self.num_sections = num_sections
+        self.prjs = simple_projections(num_sections)
+
+    def __call__(self, img):
+        return self.project(img)
+
+    def project(self, img):
+        assert img.ndim >= 3
+        prjs = [prj(img) for prj in self.prjs]
+        if img.ndim == 3:
+            tilt_series = np.stack(prjs)
+        else:
+            tilt_series = np.concatenate(prjs, axis=-4)
+        return tilt_series
+
+        
 class TiltSeries(Augment):
     """
     Tilt-series projections.
     """
     def __init__(self, num_sections):
         self.num_sections = num_sections
-        self.imgs = []
+        self.pad = num_sections // 2
+        self.sts = SimpleTiltSeries(num_sections)
+        self.imgs = []        
 
-    def prepare(self, spec, **kwargs):
-        
-
+    def prepare(self, spec, imgs=[], **kwargs):
+        self.imgs = self.__validate(spec, imgs)
+        pad = 2*self.pad
         # Update spec
         spec = dict(spec)
-        for k, v in spec.items():
-            pivot = -3
-            new_z = v[pivot] + self.nsec
-            spec[k] = v[:pivot] + (new_z,) + v[pivot+1:]
+        for k in self.imgs:
+            v = spec[k]
+            spec[k] = v[:-2] + (v[-2]+pad, v[-1]+pad)
         return spec
 
     def __call__(self, sample, **kwargs):
-        pass
+        sample = Augment.to_tensor(sample)        
+        for k in self.imgs:
+            img = sample[k]            
+            assert img.shape[-4] == 1
+            p = self.pad
+            sample[k] = self.sts(img)[..., p:-p, p:-p]
+        return Augment.sort(sample)
 
     def __repr__(self):
-        format_string = self.__class__.__name__ + '()'
+        format_string = self.__class__.__name__ + '('
+        format_string += f'num_sections={self.num_sections}'
+        format_string += ')'
         return format_string
 
-    def _validate(self, spec, imgs):
+    def __validate(self, spec, imgs):
         assert len(imgs) > 0
         assert all(k in spec for k in imgs)
         return imgs
