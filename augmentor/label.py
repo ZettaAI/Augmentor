872bf3ee9c46dc6d56707bb4115ee53a7153f5af
Kisuk Lee <kisuk@zetta.ai>
2023-11-15 11:41:18 +0900

    feat(label): add support for specifying targets

--- augmentor/label.py
+++ augmentor/label.py
@@ -10,9 +10,10 @@ class Label(Augment):
     """
     Recompute connected components.
     """
-    def __init__(self, vec=False):
-        self.vec = vec
+    def __init__(self, targets=[], vec=False):
         self.segs = []
+        self.targets = targets if targets else []
+        self.vec = vec
 
     def prepare(self, spec, segs=[], **kwargs):
         self.segs = segs
@@ -20,13 +21,15 @@ class Label(Augment):
 
     def __call__(self, sample, **kwargs):
         sample = Augment.to_tensor(sample)        
-        for k in self.segs:
+        targets = self.targets if self.targets else self.segs
+        for k in targets:
             if k in sample:
                 seg = sample[k][0,:,:,:].astype(np.uint32)
                 split = measure.label(seg).astype(np.uint32)
                 sample[k + '_split'] = split
                 if self.vec:
                     sample[k + '_split_vec'] = self.vectorize(split)
+        self.segs = []
         return Augment.sort(Augment.to_tensor(sample))
 
     def __repr__(self):
